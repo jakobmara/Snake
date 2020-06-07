498299a3520be9bf074cf9ebb182bd4300890797
jakobmara <gr8t1999@gmail.com>
2020-06-07 12:03:44 -0400

    added border and WIP with snake size

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ BLACK = (0,0,0)
 WHITE=(255,255,255)
 GREEN = (0,255,0)
 YELLOW = (255,255,0)
-
+RED = (255,0,0)
 
 HEIGHT = 704
 WIDTH = 512
@@ -41,10 +41,10 @@ def delSquare(posX,posY):
     pygame.draw.rect(gameDisplay, BLACK,(posX*16,posY*16,15,15))
 
 def randSquare():
-    pointX = randint(0,44)
-    pointY = randint(0,32)
+    pointX = randint(0,43) #had to go 1 less trhan max cuz off screen
+    pointY = randint(0,31)
     print(f"x: {pointX} y: {pointY}")
-    drawSquare(5,5)
+    drawSquare(pointX,pointY)
     return (pointX,pointY)
 
 def gameLoop():
@@ -66,11 +66,14 @@ def gameLoop():
 
 
     #pygame.draw.rect(gameDisplay,WHITE,(100,100,16,16))
+    point_location = [0,0]
 
 
     player = Snake()
+    score = player.size - 1 #cuz size starts at 1
+    playerScore = "Score %d" % score
+    scoreText = smallFont.render(playerScore,True,RED)
     while playing:
-        point_location = [0,0]
         #updates screen
         mouse = pygame.mouse.get_pos()
         diff =  pygame.time.get_ticks()
@@ -83,13 +86,29 @@ def gameLoop():
             else:
                 startText = smallFont.render("Start",True,WHITE)
 
-
+            
             gameDisplay.blit(startText,startRect)
 
         pygame.display.flip()
         
-       
+        print(f"player: {player.pos[0]} point: {point_location}")
+
+        if(player.pos[0][0] == point_location[0] and player.pos[0][1] == point_location[1]): #if player lands on point
+            point_location = randSquare() #new point location is setup
+            player.add_square()
+            #player.size += 1
+            
+            scoreText = smallFont.render(playerScore,True,BLACK) #removes old text
+            gameDisplay.blit(scoreText,[600,0])
+
+            playerScore = "Score %d" % (player.size -1) #updates text with new score
+            
+
+            scoreText = smallFont.render(playerScore,True,RED) #displays new text
+            gameDisplay.blit(scoreText,[600,0])
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 playing = False
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -97,10 +116,10 @@ def gameLoop():
                     print(f"Calling game loop... {mode}")
                    
                     player.spawn_snake()
-                    point_location = randSquare()
                     mode = "playing"
                     screen.fill(BLACK)
-
+                    point_location = randSquare()
+                    gameDisplay.blit(scoreText,[600,0])
                     pygame.display.flip()
 
                    #calls the actual game play
@@ -113,13 +132,13 @@ def gameLoop():
                     player.direction = "NORTH"
                 elif(event.key == pygame.K_DOWN):
                     player.direction = "SOUTH"
-        if (diff >= oldDiff+60 and mode == "playing"):
+        
+        if (diff >= oldDiff+60 and mode == "playing"): #this is the refresh rate
             oldDiff = diff
             player.moveSnake()
             #print(f"ticks: {diff}")
-        if(player.pos[0] == point_location):
-            print("POINT!!")
-        clock.tick(60)
+    
+    clock.tick(60)
 
 
 #how should i do this snake game? make it a board and seperate it into blocks? 
@@ -127,41 +146,59 @@ def gameLoop():
 class Snake:
 
     def __init__(self):
-        self.size = 3
+        self.size = 1
         self.speed = 1 #might not include speed
         self.pos = [[21,16]] #coords of the snake's head
         self.direction = "NORTH"
 
 
     def add_square(self):
-        self.size += 1
+        newCoords = [self.pos[self.size-1][0] - 1, self.pos[self.size-1][1] - 1] #finding position of new square
+        print(f"ADDED: {self.pos}")
+        self.pos.append(newCoords) #adding it to the list of square positions
+        drawSquare(newCoords[0],newCoords[1]) #drawing it on screen
+        self.size += 1 #incrementing score/size
+        pass
+
 
     def spawn_snake(self):
         drawSquare(21,16)
         #drawSquare(22,16)
         #drawSquare(23,16)
     
-    def moveSnake(self):
+    def moveSnake(self): #make a for loop that goes through the list of positions and updates all of them depending on direction
         if self.direction == "NORTH":
-            delSquare(self.pos[0][0],self.pos[0][1])
-            self.pos[0][1] = self.pos[0][1] - 1
+            delSquare(self.pos[self.size-1][0],self.pos[self.size-1][1]) #delsquare removes the last square on the snake
+            if self.pos[0][1] == 0: #reached border
+                self.pos[0][1] = 0
+            else:
+                self.pos[0][1] = self.pos[0][1] - 1
             drawSquare(self.pos[0][0],self.pos[0][1])
 
         elif self.direction == "SOUTH":
-            delSquare(self.pos[0][0],self.pos[0][1])
-            self.pos[0][1] = self.pos[0][1] + 1
+            delSquare(self.pos[self.size-1][0],self.pos[self.size-1][1])
+            if self.pos[0][1] == GHEIGHT - 1: #reached border
+                self.pos[0][1] = GHEIGHT - 1
+            else:
+                self.pos[0][1] = self.pos[0][1] + 1
             drawSquare(self.pos[0][0],self.pos[0][1])
 
         elif self.direction == "EAST":
-            delSquare(self.pos[0][0],self.pos[0][1])
-            self.pos[0][0] = self.pos[0][0] + 1
+            delSquare(self.pos[self.size-1][0],self.pos[self.size-1][1]) #can proll move this to the else
+            if self.pos[0][0] == GLENGTH - 1:
+                self.pos[0][0] = GLENGTH - 1
+            else:
+                self.pos[0][0] = self.pos[0][0] + 1
             drawSquare(self.pos[0][0],self.pos[0][1])
 
         elif self.direction == "WEST":
-            delSquare(self.pos[0][0],self.pos[0][1])
-            self.pos[0][0] = self.pos[0][0] - 1
+            delSquare(self.pos[self.size-1][0],self.pos[self.size-1][1])
+            if self.pos[0][0] == 0:
+                self.pos[0][0] = 0
+            else:
+                self.pos[0][0] = self.pos[0][0] - 1
             drawSquare(self.pos[0][0],self.pos[0][1])
-
+        
         pass
 
 def game_Menu():
